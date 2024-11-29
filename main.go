91827b8545a9620b09b2bc684e9cbe3d3772fb3f
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-11-29 16:53:31 +0700

    Update main.go Add if ... else if ... else Statement

--- main.go
+++ main.go
@@ -12,8 +12,17 @@ func main() {
 
     // Golang - if...else Statement
     // x := 100
-
     // if x == 100 {
+    //     fmt.Println("Japan")
+    // } else {
+    //     fmt.Println("Canada")
+    // }
+
+    // Golanng if... else if ... else Statement
+    // x := 100
+    // if x == 50 {
+    //     fmt.Println("Germany")
+    // } else if x == 100 {
     //     fmt.Println("Japan")
     // } else {
     //     fmt.Println("Canada")
