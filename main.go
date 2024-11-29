b380583611f62a7cba30d5039c9ec78f8352511f
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-11-29 16:50:07 +0700

    Update main.go Add if...else Statement

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-    // Golang - If Statement
+    // Golang - if Statement
     // var s = "Japan"
     // x := true
     // if x {
     //     fmt.Println(s)
+    // }
+
+    // Golang - if...else Statement
+    // x := 100
+
+    // if x == 100 {
+    //     fmt.Println("Japan")
+    // } else {
+    //     fmt.Println("Canada")
     // }
 }
